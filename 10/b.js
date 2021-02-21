0d6739782707a2d3d18cc835ecd5bb37b6309f79
matt <mattclarkeruns@gmail.com>
2021-02-21 13:44:56 -0500

    Day 10 part b

---
+++ 10/b.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var lookAndSay_1 = require("../models/lookAndSay");
+var input = 1113222113;
+var lookAndSay = new lookAndSay_1.LookAndSay(input);
+console.log(lookAndSay.getNthNumber(50).length);
